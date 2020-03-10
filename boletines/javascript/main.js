65b72adc80d4883c83c9f6ff5c2cd17fd3fb7b11
Hnenry <enrique_g_anaya@hotmail.com>
2020-03-10 14:50:40 -0600

    Se agregó el proyecto "boletines"

---
+++ boletines/javascript/main.js
@@ -0,0 +1,31 @@
+
+//Almacenar en "x" todos los elementos de la clase .close
+let x = document.querySelectorAll(".close");
+
+//Recorrer x 
+x.forEach(function(link) {
+
+//Agregar un evento "click" a cada elemento link
+    link.addEventListener("click",function(ev){
+      ev.preventDefault();
+
+      let content = document.querySelector('.content');
+
+//Quitar las clases de animación con classList
+        content.classList.remove("fadeInUp");
+        content.classList.remove("animated");
+
+//Agregar clases para animar su salida faceOutUp
+        content.classList.add("fadeOutUp");
+        content.classList.add("animated");
+
+//Dar 6 milisegundos para leer la instrucción de redirección del link         
+        setTimeout(() => {
+            location.href="/";
+        }, 600);
+
+        return false
+    });
+    
+});
+
